667e9392086708dd2d3f58bf6643c08cd4ad2682
Paula Gramajo <paulagramajo.tuc@gmail.com>
2025-06-09 20:46:49 -0300

    realice el ejercicio 5 de la practica

---
+++ js/5-ejercicioCinco.js
@@ -0,0 +1,3 @@
+let num1 = parseInt(window.prompt("Ingresa un número"));
+let num2 = parseInt(window.prompt("Ingrese otro número"));
+document.writeln("La suma de los dos números es igual a: " +(num1+num2));
